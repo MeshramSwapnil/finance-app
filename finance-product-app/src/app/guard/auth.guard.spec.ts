a202148680ce6a677972b8460b9ba545a7b6f10a
SWAPNIL MESHRAM <nagarro.meshram@extaccount.com>
2023-03-21 01:07:17 +0530

    Added Tests
    Added Tests

--- finance-product-app/src/app/guard/auth.guard.spec.ts
+++ finance-product-app/src/app/guard/auth.guard.spec.ts
@@ -1,21 +1,47 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, of } from 'rxjs';
 import { AuthenticationService } from '../services/auth/authentication.service';
 
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
+  let authService : AuthenticationService;
+  let router = jasmine.createSpyObj('Router',['navigateByUrl'])
+
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports : [HttpClientTestingModule],
-      providers : [AuthenticationService]
+      providers : [AuthenticationService,{provide: Router, useValue: router}]
     });
     guard = TestBed.inject(AuthGuard);
+    authService = TestBed.inject(AuthenticationService);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  it('user is autheticated and allowed to route', () => {
+    authService.isAuthenticated = true;
+    expect(guard.canActivate(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{url: 'testUrl'})).toBeDefined();
+    expect(guard.canActivate(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{url: 'testUrl'})).toBeTruthy()
+    // @ts-ignore
+    guard.canActivate(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{url: 'testUrl'}).subscribe((res) => {
+      expect(res).toBe(true);
+    });
+  });
+
+  it('user is unautheticated and not allowed to route', () => {
+    authService.isAuthenticated = false;
+    expect(guard.canActivate(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{url: 'testUrl'})).toBeInstanceOf(Observable);
+    expect(router.navigateByUrl).toHaveBeenCalled();
+    // @ts-ignore
+    guard.canActivate(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{url: 'testUrl'}).subscribe((res) => {
+      expect(res).toBe(false);
+    });
   });
 });
--- finance-product-app/src/app/interceptors/authentication.interceptor.spec.ts
+++ finance-product-app/src/app/interceptors/authentication.interceptor.spec.ts
@@ -1,23 +1,45 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { AuthenticationService } from '../services/auth/authentication.service';
 
 import { AuthenticationInterceptor } from './authentication.interceptor';
 
 describe('AuthenticationInterceptor', () => {
 
-  beforeEach(() => TestBed.configureTestingModule({
-    imports : [
-      HttpClientTestingModule
-    ],
-    providers: [
-      AuthenticationInterceptor,
-      AuthenticationService
-      ]
-  }));
+  let authService = jasmine.createSpyObj('AuthenticationService',{
+    'getToken' : 'token'
+  })
+  let request = jasmine.createSpyObj('HttpRequest',['clone'])
+  let next = jasmine.createSpyObj('HttpHandler',['handle'])
+
+  let interceptor : AuthenticationInterceptor;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports : [
+        HttpClientTestingModule
+      ],
+      providers: [
+        AuthenticationInterceptor,
+        {
+          provide : AuthenticationService,
+          useValue : authService
+        }
+        ]
+    })
+     interceptor = TestBed.inject(AuthenticationInterceptor);
+  });
 
   it('should be created', () => {
-    const interceptor: AuthenticationInterceptor = TestBed.inject(AuthenticationInterceptor);
     expect(interceptor).toBeTruthy();
   });
+
+  it('should be intercept', () => {
+    next.handle.and.returnValue(of({}));
+    expect(interceptor.intercept(request,next)).toBeTruthy();
+    expect(request.clone).toHaveBeenCalled();
+    expect(next.handle).toHaveBeenCalled();
+  });
+
 });
