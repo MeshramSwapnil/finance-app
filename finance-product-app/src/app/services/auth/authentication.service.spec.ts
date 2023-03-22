4db893330856e182f1cda873ece9ba0242997bb7
SWAPNIL MESHRAM <nagarro.meshram@extaccount.com>
2023-03-22 20:03:03 +0530

    Added Tests
    Added Tests

--- finance-product-app/src/app/services/auth/authentication.service.spec.ts
+++ finance-product-app/src/app/services/auth/authentication.service.spec.ts
@@ -1,14 +1,22 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { AuthenticationService } from './authentication.service';
 
 describe('AuthenticationService', () => {
   let service: AuthenticationService;
-
+  let httpClient = jasmine.createSpyObj('HttpClient',{
+    'post' : of({}),
+    'get' : of(new HttpResponse<Object>())
+  });
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ HttpClientTestingModule ]
+      imports: [ HttpClientTestingModule ],
+      providers: [{
+        provide: HttpClient, useValue: httpClient
+      }]
     });
     service = TestBed.inject(AuthenticationService);
   });
@@ -16,4 +24,18 @@ describe('AuthenticationService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('check do login', () => {
+    let formData = new FormData();
+    service.doLogin(formData);
+    expect(httpClient.post).toHaveBeenCalled()
+  });
+
+  it('check fetchRefreshToken', () => {
+    service.setRefreshToken('Token');
+    service.fetchRefreshToken();
+    httpClient.get.and.returnValue(of(new HttpResponse<Object>()))
+    expect(httpClient.get).toHaveBeenCalled()
+  });
+
 });
